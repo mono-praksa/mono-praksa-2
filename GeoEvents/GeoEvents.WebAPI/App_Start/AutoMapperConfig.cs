a1c489c0a29201750950c6b1dd3ee44669df6180
Zvonimir <zvonimir.stipanovic1@gmail.com>
2017-07-05 12:13:54 +0200

    added automapper for images

--- GeoEvents/GeoEvents.WebAPI/App_Start/AutoMapperConfig.cs
+++ GeoEvents/GeoEvents.WebAPI/App_Start/AutoMapperConfig.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using GeoEvents.Model.Common;
+using GeoEvents.Repository.Common;
 
 namespace GeoEvents.WebAPI.App_Start
 {
@@ -12,8 +14,8 @@ namespace GeoEvents.WebAPI.App_Start
             AutoMapper.Mapper.Initialize(config =>
             {
                 config.CreateMap<IEventEntity, IEvent>().ReverseMap();
+                config.CreateMap<IImageEntity, IImage>().ReverseMap();
             });
-
         }
     }
 }
