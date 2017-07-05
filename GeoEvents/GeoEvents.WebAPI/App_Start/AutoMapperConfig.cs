f7e9f062d6d77c97d90975f0e72bae630e900f51
Zvonimir <zvonimir.stipanovic1@gmail.com>
2017-07-05 11:42:57 +0200

    added automapperconfig

---
+++ GeoEvents/GeoEvents.WebAPI/App_Start/AutoMapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GeoEvents.WebAPI.App_Start
+{
+    public class AutoMapperConfig
+    {
+        public static void Initialize()
+        {
+            AutoMapper.Mapper.Initialize(config =>
+            {
+                config.CreateMap<IEventEntity, IEvent>().ReverseMap();
+            });
+
+        }
+    }
+}
