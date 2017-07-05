084b96dbfe63e8c8c8c6d67db0b73e79c5600541
IvanBacic <ivanshowtekwe@gmail.com>
2017-07-05 11:52:44 +0200

    Repo+DAL

---
+++ GeoEvents/GeoEvents.DAL/EventEntity.cs
@@ -0,0 +1,39 @@
+﻿using GeoEvents.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoEvents.DAL
+{
+    public class EventEntity : IEventEntity
+    {
+        public Guid Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public Decimal Lat { get; set; }
+        public Decimal Long { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Category { get; set; }
+
+        public EventEntity() { }
+
+
+        public EventEntity(Guid Id, DateTime StartTime, DateTime EndTime, Decimal Lat,
+            Decimal Long, string Name, string Description, int Category)
+        {
+            this.Id = Id;
+            this.StartTime = StartTime;
+            this.EndTime = EndTime;
+            this.Lat = Lat;
+            this.Long = Long;
+            this.Name = Name;
+            this.Description = Description;
+            this.Category = Category;
+        }
+
+    }
+}
+
