13f8bf2c53f1783b18b59501654df41c34d695e7
dsuhic <dsuhic@mathos.hr>
2017-07-05 11:47:15 +0200

    Service/Common layers push.

---
+++ GeoEvents/GeoEvents.Model.Common/IEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoEvents.Model.Common
+{
+    public interface IEvent
+    {
+        #region Properties
+
+        Guid Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        decimal Lat { get; set; }
+        decimal Long { get; set; }
+        List<int> Categories { get; set; }
+        DateTime StartTime { get; set; }
+        DateTime EndTime { get; set; }
+        int Category { get; set; }
+
+        #endregion Properties
+    }
+}
