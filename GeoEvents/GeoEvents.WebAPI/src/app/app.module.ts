73f641475c7c39a4bcae71477ee179a2b2b67204
Zvonimir <zvonimir.stipanovic1@gmail.com>
2017-07-06 16:43:46 +0200

    created partialy create event component

--- GeoEvents/GeoEvents.WebAPI/src/app/app.module.ts
+++ GeoEvents/GeoEvents.WebAPI/src/app/app.module.ts
@@ -1,11 +1,21 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }      from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent }  from './app.component';
+import { AppComponent } from './app.component'
+import { CreateEventComponent } from './create-event.component'
+import { NguiDatetimePickerModule } from '@ngui/datetime-picker'
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        NguiDatetimePickerModule
+    ],
+    declarations: [
+        AppComponent,
+        CreateEventComponent
+    ],
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule { }
