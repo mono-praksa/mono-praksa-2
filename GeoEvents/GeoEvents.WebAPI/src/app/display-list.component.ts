3ab9cdb7389e579cd6d173b53f94962cac8c1ea7
Ivana <ivana.pristavnik@gmail.com>
2017-07-10 09:57:22 +0200

    Display list component

--- GeoEvents/GeoEvents.WebAPI/src/app/display-list.component.ts
+++ GeoEvents/GeoEvents.WebAPI/src/app/display-list.component.ts
@@ -1,7 +1,52 @@
-﻿import { Component } from '@angular/core'
+﻿import { Component, OnInit, ElementRef, NgZone, ViewChild } from '@angular/core'
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
+import { Http, Response, Headers, RequestOptions } from '@angular/http'
+import { Observable } from 'rxjs/Rx'
+import { MapsAPILoader } from '@agm/core'
 
 @Component({
     templateUrl: "app/display-list.component.html"
 })
-export class DisplayListComponent {
+export class DisplayListComponent implements OnInit {
+
+    categories: any[] = [
+        { id: 1, checked: false },
+        { id: 2, checked: false },
+        { id: 4, checked: false },
+        { id: 8, checked: false },
+        { id: 16, checked: false },
+        { id: 32, checked: false },
+        { id: 64, checked: false }
+    ]
+    filterForm: FormGroup
+
+    start: FormControl
+    end: FormControl
+    category: FormControl
+    address: FormControl
+    radius: FormControl
+
+    constructor(private http: Http, private formBuilder: FormBuilder, private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) { }
+
+    ngOnInit() {
+        this.start = new FormControl('');
+        this.end = new FormControl('');
+        this.address = new FormControl('');
+        this.radius = new FormControl('');
+
+        this.filterForm = this.formBuilder.group({
+            start: this.start,
+            end: this.end,
+            address: this.address,
+            radius: this.radius
+        });
+
+    }
+    updateCategories(category: number) {
+        this.categories.filter(checkbox => {
+            if (checkbox.id == category) {
+                checkbox.checked = !checkbox.checked;
+            }
+        });
+    }
 }
