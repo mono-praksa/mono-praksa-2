97aaad025ebbc30008c6d2d63760c9802c29e67b
dsuhic <dsuhic@mathos.hr>
2017-07-10 12:35:43 +0200

    Add Event and Image interfaces to Angular.

--- GeoEvents/GeoEvents.WebAPI/src/app/event-details.component.ts
+++ GeoEvents/GeoEvents.WebAPI/src/app/event-details.component.ts
@@ -1,9 +1,35 @@
-﻿import { Component } from '@angular/core'
+﻿import { Component, Input, OnInit } from '@angular/core'
+import { IEvent } from './event.model'
+import { IImage } from './image.model'
+import { Http, Response, Headers, RequestOptions } from '@angular/http'
+import { Observable } from 'rxjs/Rx'
 
 @Component({
-    template: ''
+    templateUrl: 'app/event-details.component.html'
 })
 
-export class EventDetailsComponent {
+export class EventDetailsComponent implements OnInit{
+    @Input() event: IEvent
+    images: IImage[]
+
+    constructor(private http: Http) {
+
+    }
+
+    ngOnInit() {
+        this.getImages(this.event.Id).subscribe(res => {
+            this.images = res
+        })
+    }
+
+    getImages(id : number): Observable<IImage[]> {
+        return this.http.get('/api/image/get/' + this.event.Id).map(function (response: Response) {
+            return <IImage[]>response.json();
+        }).catch(this.handleError);
+    }
+
+    handleError(error: Response) {
+        return Observable.throw(error.statusText);
+    }
 
 }
