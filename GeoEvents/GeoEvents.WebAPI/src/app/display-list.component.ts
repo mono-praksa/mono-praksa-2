c01f0b6f2406665706b5baa981a7aaa3965b3ea0
Ivana <ivana.pristavnik@gmail.com>
2017-07-10 10:19:37 +0200

    Search events update

--- GeoEvents/GeoEvents.WebAPI/src/app/display-list.component.ts
+++ GeoEvents/GeoEvents.WebAPI/src/app/display-list.component.ts
@@ -1,52 +1,10 @@
-﻿import { Component, OnInit, ElementRef, NgZone, ViewChild } from '@angular/core'
-import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms'
-import { Http, Response, Headers, RequestOptions } from '@angular/http'
-import { Observable } from 'rxjs/Rx'
-import { MapsAPILoader } from '@agm/core'
+﻿import { Component } from '@angular/core'
 
 @Component({
-    templateUrl: "app/display-list.component.html"
+    template: '<h1>I AM DISPLAY LIST COMPONENT</h1>',
+    selector: 'display-list'
 })
-export class DisplayListComponent implements OnInit {
 
-    categories: any[] = [
-        { id: 1, checked: false },
-        { id: 2, checked: false },
-        { id: 4, checked: false },
-        { id: 8, checked: false },
-        { id: 16, checked: false },
-        { id: 32, checked: false },
-        { id: 64, checked: false }
-    ]
-    filterForm: FormGroup
+export class DisplayListComponent {
 
-    start: FormControl
-    end: FormControl
-    category: FormControl
-    address: FormControl
-    radius: FormControl
-
-    constructor(private http: Http, private formBuilder: FormBuilder, private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) { }
-
-    ngOnInit() {
-        this.start = new FormControl('');
-        this.end = new FormControl('');
-        this.address = new FormControl('');
-        this.radius = new FormControl('');
-
-        this.filterForm = this.formBuilder.group({
-            start: this.start,
-            end: this.end,
-            address: this.address,
-            radius: this.radius
-        });
-
-    }
-    updateCategories(category: number) {
-        this.categories.filter(checkbox => {
-            if (checkbox.id == category) {
-                checkbox.checked = !checkbox.checked;
-            }
-        });
-    }
 }
--- GeoEvents/GeoEvents.WebAPI/src/app/routes.ts
+++ GeoEvents/GeoEvents.WebAPI/src/app/routes.ts
@@ -1,12 +1,12 @@
 ﻿import { Routes } from '@angular/router'
 
 import { CreateEventComponent } from './create-event.component'
-import { DisplayListComponent } from './display-list.component'
+import { SearchEventsComponent } from './search-events.component'
 import { Error404Component } from './errors/error-404.component'
 
 export const routes: Routes = [
     { path: 'create', component: CreateEventComponent },
-    { path: 'display/list', component: DisplayListComponent },
+    { path: 'search', component: SearchEventsComponent },
     { path: '', redirectTo: 'create', pathMatch: 'full' },
     { path: '404', component: Error404Component },
     { path: '**', redirectTo: '404' }
