325e663fb89325a27595200898ca1eea380e21b4
Ivana <ivana.pristavnik@gmail.com>
2017-07-12 14:04:15 +0200

    Add map.

--- GeoEvents/GeoEvents.WebAPI/src/app/display/display-map.component.ts
+++ GeoEvents/GeoEvents.WebAPI/src/app/display/display-map.component.ts
@@ -1,18 +1,23 @@
 ﻿import { Component, Output, EventEmitter, Input } from '@angular/core'
 import { IEvent } from './../models/event.model'
+import { MapsAPILoader } from '@agm/core'
 
 @Component({
-    template: '<h1>I AM DISPLAY MAP COMPONENT</h1>',
     selector: 'display-map',
     styles: [`
-        agm-map {
-            height: 300px;
-        }
-    `]
+    agm-map {
+      height: 500px;
+    }
+  `],
+    template: `
+  <agm-map [latitude]="lat" [longitude]="lng"></agm-map>
+  `
 })
 
 export class DisplayMapComponent {
     @Input() events: IEvent[]
+    lat: number = 51.678418;
+    lng: number = 7.809007;
     @Output() event = new EventEmitter()
 
     displayEvent(evt: IEvent) {
