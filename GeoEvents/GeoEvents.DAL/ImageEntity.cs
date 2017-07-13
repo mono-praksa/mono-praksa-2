2e915c47e3ab6fa654d331275af5ac665e2934c1
spjadic <grgic.martina1@gmail.com>
2017-07-13 12:21:59 +0200

    comments for database models

--- GeoEvents/GeoEvents.DAL/ImageEntity.cs
+++ GeoEvents/GeoEvents.DAL/ImageEntity.cs
@@ -7,25 +7,46 @@ using System.Threading.Tasks;
 
 namespace GeoEvents.DAL
 {
+    /// <summary>
+    /// Database model ImageEntity
+    /// </summary>
     public class ImageEntity 
     {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the content of the image.
+        /// </summary>
+        /// <value>
+        /// The content of the image.
+        /// </value>
         public byte[] Content { get; set; }
 
+        /// <summary>
+        /// Gets or sets the event identifier.
+        /// </summary>
+        /// <value>
+        /// The event identifier.
+        /// </value>
         public Guid EventId { get; set; }
+        #endregion Properties
 
-
+        #region Constructors
+        public ImageEntity() { }
         public ImageEntity(Guid Id, byte[] Content, Guid EventId)
         {
             this.Id = Id;
             this.Content = Content;
             this.EventId = EventId;
         }
-
-        public ImageEntity()
-        {
-
-        }
+        #endregion Constructors
 
     }
 }
