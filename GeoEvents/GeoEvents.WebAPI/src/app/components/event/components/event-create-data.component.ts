196607797000e943ca2da547b0d34cde66e65b81
imilicic <imilicic@mathos.hr>
2017-07-17 16:22:20 +0200

    added loader service and loading to create event component

--- GeoEvents/GeoEvents.WebAPI/src/app/components/event/components/event-create-data.component.ts
+++ GeoEvents/GeoEvents.WebAPI/src/app/components/event/components/event-create-data.component.ts
@@ -8,6 +8,7 @@ import { Router } from '@angular/router'
 import { endDateBeforeStartDate } from '../validators/validator'
 import { IEvent } from '../models/event.model'
 import { CategoryEnum } from '../../../shared/common/category-enum'
+import { LoaderService } from '../../../shared/loader.service'
 
 @Component({
     selector: "create-event",
@@ -24,6 +25,7 @@ export class EventCreateDataComponent implements OnInit {
     private _latitude: number;
     private _longitude: number;
     private _zoom: number;
+    createEventLoading: boolean = false;
 
     private _createdEvent: IEvent;
 
@@ -49,7 +51,14 @@ export class EventCreateDataComponent implements OnInit {
     end: FormControl
     category: FormControl
 
-    constructor(private http: Http, private formBuilder: FormBuilder, private mapsAPILoader: MapsAPILoader, private ngZone: NgZone, private router: Router) { }
+    constructor(
+        private http: Http,
+        private formBuilder: FormBuilder,
+        private mapsAPILoader: MapsAPILoader,
+        private ngZone: NgZone,
+        private router: Router,
+        private _loaderService: LoaderService
+    ) { }
 
     ngOnInit(): void {
         this.name = new FormControl('', Validators.required);
@@ -63,6 +72,10 @@ export class EventCreateDataComponent implements OnInit {
             start: this.start,
             end: this.end
         }, { validator: endDateBeforeStartDate('start', 'end') });
+
+        this._loaderService.loaderStatus.subscribe((value: boolean) => {
+            this.createEventLoading = value;
+        })
 
         //GOOGLE MAPS
         this.zoom = 4;
@@ -110,6 +123,7 @@ export class EventCreateDataComponent implements OnInit {
 
     createEvent(formValues: any) {
         this.creatingEvent = true;
+        this._loaderService.displayLoader(true);
         let chosenCategories: number[] = [];
         this.categories.filter(checkbox => {
             if (checkbox.checked) {
@@ -135,6 +149,7 @@ export class EventCreateDataComponent implements OnInit {
         }).catch(this.handleError).subscribe((response: Response) => {
             this.createdEvent = <IEvent>response.json();
             this.eventEmitter.emit(this.createdEvent);
+            this._loaderService.displayLoader(false);
         });
     }
 
