5b27f276a5cebbe275a821b4f607c9457acb5e5a
imilicic <imilicic@mathos.hr>
2017-07-20 09:22:51 +0200

    fixed paginator and filter form

--- GeoEvents/GeoEvents.WebAPI/src/app/components/event/components/event-search-list.component.ts
+++ GeoEvents/GeoEvents.WebAPI/src/app/components/event/components/event-search-list.component.ts
@@ -5,16 +5,13 @@ import { IEvent } from '../models/event.model';
 
 @Component({
     templateUrl:'app/components/event/views/event-search-list.component.html',
-    selector: 'display-list',
-    styleUrls: [
-        '../../../../../node_modules/primeng/resources/primeng.min.css',
-        '../node_modules/primeng/resources/themes/omega/theme.css'
-    ]
+    selector: 'display-list'
 })
 
 export class EventListComponent {
-    @Input() events : IEvent[]
-    @Output() event = new EventEmitter()
+    @Input() events: IEvent[];
+    @Input() eventCount: number = 0;
+    @Output() event = new EventEmitter();
 
     eventDetails(evt: IEvent) {
         this.event.emit(evt);
